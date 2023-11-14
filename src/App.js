43b32eb969df6442c9875b07292072066a377457
Faris Saputra <farissaputra375@gmail.com>
2023-11-14 16:36:02 +0700

    dokumentasi API

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
+import Select from "react-select";
 
 function App() {
+  const [datas, setDatas] = useState([]);
+  const [userSelect, setUserSelect] = useState("");
+  const [isShow, setIsShow] = useState(false);
+
+  const getBerries = async () => {
+    const berry = await fetch("https://pokeapi.co/api/v2/berry/");
+    const value = await berry.json();
+    let result = value.results.map((data) => {
+      return {
+        value: data.name,
+        label: data.name,
+      };
+    });
+    setDatas(result.sort((a, b) => a.label.localeCompare(b.label)));
+  };
+
+  useEffect(() => {
+    getBerries();
+  }, []);
+
+  const handleSumbit = () => {
+    setIsShow((state) => !state);
+  };
+
+  const handleChange = (value) => {
+    setUserSelect(value);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>{isShow ? userSelect : ""}</h1>
+      <button onClick={() => handleSumbit()} disabled={!userSelect}>
+        {isShow ? "Hide Button" : "Show Values"}
+      </button>
+      <br />
+      <br />
+      <Select options={datas} onChange={(e) => handleChange(e.value)} />
     </div>
   );
 }
